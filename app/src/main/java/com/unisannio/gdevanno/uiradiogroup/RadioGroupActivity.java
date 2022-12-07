a1a790e7142f4febe41071faba751e627f48e8a2
Giovanni De Vanno <giovannidv96@gmail.com>
2022-12-07 12:56:22 +0100

    implementata la classe RadioGroupActivity

--- app/src/main/java/com/unisannio/gdevanno/uiradiogroup/RadioGroupActivity.java
+++ app/src/main/java/com/unisannio/gdevanno/uiradiogroup/RadioGroupActivity.java
@@ -3,6 +3,11 @@ package com.unisannio.gdevanno.uiradiogroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
 
 public class RadioGroupActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class RadioGroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio_group);
+
+        final TextView tv = (TextView) findViewById(R.id.textView);
+
+        //Define a generic listener for all three RadioButtons in the RadioGroup
+        final View.OnClickListener radioListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioButton rb = (RadioButton) view;
+                tv.setText(rb.getText() + "chosen");
+            }
+        };
+
+        final RadioButton choice1 = (RadioButton) findViewById(R.id.choice1);
+        //Called when RadioButton choice1 is clicked
+        choice1.setOnClickListener(radioListener);
+
+        final RadioButton choice2 = (RadioButton) findViewById(R.id.choice2);
+        //Called when RadioButton choice2 is clicked
+        choice2.setOnClickListener(radioListener);
+
+        final RadioButton choice3 = (RadioButton) findViewById(R.id.choice3);
+        //Called when RadioButton choice3 is clicked
+        choice3.setOnClickListener(radioListener);
+
     }
 }
